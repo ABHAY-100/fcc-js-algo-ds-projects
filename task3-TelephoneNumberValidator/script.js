f75ae8642adb64e03d592dc792508791aa1dd8d0
ABHAY-100 <abhaybalakrishnan884@gmail.com>
2024-06-14 10:56:20 +0530

    Add task3
    Completed the ui

---
+++ task3-TelephoneNumberValidator/script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+    const userInput = document.getElementById("user-input");
+    const checkBtn = document.getElementById("check-btn");
+    const clearBtn = document.getElementById("clear-btn");
+})
